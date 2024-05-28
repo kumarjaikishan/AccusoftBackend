a891dd43ebedf5fca3e0b9b9cde908d218361dae
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-05-28 21:08:08 +0530

    ledger

--- controller/ledger_controller.js
+++ controller/ledger_controller.js
@@ -10,11 +10,16 @@ const addledger = asyncHandler(async (req, res,next) => {
         return next({ status: 400, message: "All Fields are Required" });
     }
 
+    const isExists = await ledmodel.findOne({userid:req.userid, ledger:req.body.ledger});
+    if(isExists){
+        return next({ status: 400, message: `${req.body.ledger} Already Exist` });
+    }
+
     const query = new ledmodel({ userid: req.userid, ledger: req.body.ledger });
     const result = await query.save();
-    console.log(result);
+    // console.log(result);
     return res.status(200).json({
-        message: "Ledger added Successfully"
+        message: "Ledger Added "
     })
 })
 
@@ -23,17 +28,33 @@ const addledger = asyncHandler(async (req, res,next) => {
 // *--------------------------------------
 const updateledger = asyncHandler(async (req, res,next) => {
     const { ledger_id, newledger } = req.body;
+    console.log("old id",ledger_id);
 
     if (!ledger_id || !newledger) {
         return next({ status: 400, message: "All Fields are Required" });
     }
+
+    const isExists = await ledmodel.findOne({userid:req.userid, ledger:newledger});
+    if(isExists){
+        return next({ status: 400, message: `${newledger} Already Exist` });
+    }
+
+    //If want to merge ledgers
+    // if(isExists){
+    //     const query = await expense.updateMany({ ledger: ledger_id }, { ledger: isExists._id });
+    //     const query2 = await ledmodel.deleteOne({ _id: ledger_id });
+
+    //     return res.status(200).json({
+    //         message: "Ledger Updated"
+    //     })
+    // }
 
     const query = await ledmodel.findByIdAndUpdate({ _id: ledger_id }, { ledger: newledger });
     if (!query) {
         return next({ status: 400, message: "Ledger Id not Valid" });
     }
     return res.status(200).json({
-        message: "Ledger Updated Successfully"
+        message: "Ledger Updated"
     })
 })
 
@@ -54,9 +75,8 @@ const deleteledger = asyncHandler(async (req, res,next) => {
             return next({ status: 400, message: "Id not Valid" });
         }
         return res.status(200).json({
-            message: "Ledger Deleted Successfully"
+            message: "Ledger Deleted"
         })
-  
 })
 
 
