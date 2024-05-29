75a94bbbc27230de4c28ef2098998bdde07876fc
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-05-29 12:23:01 +0530

    xfsdf

--- controller/login_contoroller.js
+++ controller/login_contoroller.js
@@ -173,47 +173,54 @@ const login = async (req, res, next) => {
     return next({ status: 400, message: "All Fields are Required" });
   }
 
-  try {
-    const isUser = await user.find({ email });
-    if (!isUser) {
-      return next({ status: 400, message: "User not found" });
-    }
-    const generateToken = async (result) => {
-      try {
-        return jwt.sign({
-          userId: result._id.toString(),
-          email: result.email,
-          isAdmin: result.isadmin
-        },
-          process.env.jwt_token,
-          {
-            expiresIn: "30d",
-          }
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  let usersdata;
+  if (myCache.has("allusers")) {
+    usersdata = JSON.parse(myCache.get("allusers"));
+  } else {
+    usersdata = await user.find({});
+    myCache.set("allusers", JSON.stringify(usersdata));
+  }
 
-    if (await bcrypt.compare(password, isUser.password)) {
-      const dfg = await generateToken(isUser);
-      const fbf = isUser._id.toString();
-      isUser.password = undefined;
-      isUser.createdAt = undefined;
-      isUser._id = undefined;
-      isUser.phone = undefined;
-      return res.status(200).json({
-        message: "Login Successful",
-        token: dfg,
-        userId: fbf
-      });
-
-    } else {
-      return next({ status: 400, message: "Incorrect Password" });
+  const result = await usersdata.find((hel) => {
+    return hel.email == req.body.email
+  });
+  // console.log("result", result);
+  if (!result) {
+    return next({ status: 400, message: "User not found" });
+  }
+  // console.log("password match: ", await bcrypt.compare(password, result.password));
+  const generateToken = async (result) => {
+    try {
+      return jwt.sign({
+        userId: result._id.toString(),
+        email: result.email,
+        isAdmin: result.isadmin
+      },
+        process.env.jwt_token,
+        {
+          expiresIn: "30d",
+        }
+      );
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.log(error);
-    return next({ status: 400, message: error.message });
+  }
+
+  if (await bcrypt.compare(password, result.password)) {
+    const dfg = await generateToken(result);
+    const fbf = result._id.toString();
+    result.password = undefined;
+    result.createdAt = undefined;
+    result._id = undefined;
+    result.phone = undefined;
+    return res.status(200).json({
+      message: "Login Successful",
+      token: dfg,
+      userId: fbf
+    });
+
+  } else {
+    return next({ status: 400, message: "Incorrect Password" });
   }
 }
 
@@ -603,4 +610,4 @@ const verify = async (req, res) => {
   }
 }
 
-module.exports = { signup, passreset, setpassword, checkmail, photo, login, updateuserdetail, verify };
+module.exports = { signup,passreset, setpassword,checkmail, photo, login, updateuserdetail, verify };
