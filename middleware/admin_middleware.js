f971ea959cf8bedaf5abfab674e4a1f4aa54fbea
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-06-22 15:21:05 +0530

    dfrt

--- middleware/admin_middleware.js
+++ middleware/admin_middleware.js
@@ -1,6 +1,6 @@
 const adminmiddleware = async (req, res, next) => {
     // console.log(req.user.isadmin);
-    if (!req.user.isadmin) {
+    if (!req.user.isAdmin) {
       return  res.status(403).json({
         message: "Access Denied!"
         })
